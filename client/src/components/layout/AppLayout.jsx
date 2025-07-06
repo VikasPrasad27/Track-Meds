bf30afe184e2914e51d3ba00032104156fb814d0
Vikas <vjp66778899@gmail.com>
2025-07-06 17:39:52 +0530

    Update AppLayout.jsx to display chatbor

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Outlet, useLocation } from 'react-router-dom';
+import FloatingChatBot from '../FlotingChatbot';
+
 function AppLayout() {
+  const location = useLocation();
+  const hideChatBot = location.pathname === "/chatbot";
+
   return (
-    <div>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+    <div className="relative">
+      <Header />
+      <main className="min-h-[80vh]">
+        <Outlet />
+      </main>
+      <Footer />
+      {!hideChatBot && <FloatingChatBot/>}
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
